3cf98ace9d3fe3647aa4f8dc04e637cc186c7239
DanielPetelin <daniilpetelin77@gmail.com>
2020-03-25 10:10:33 +0300

    add final commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
+// функция при клике на кнопку
 document.querySelector('#push').onclick = function() {
-    let l = document.querySelector('#length').value;
+    let length = document.querySelector('#length').value;
     let min = document.querySelector('#min').value;
     let max = document.querySelector('#max').value;
     let result = [];
 
-    for (let i = 0; i < l; i++) {
+    for (let i = 0; i < length; i++) {
         result.push(randomInteger(min, max));
     }
+
     // console.log(result);
     document.querySelector('#out').innerHTML = result;
     document.querySelector('#out-input').value = result;
     return result;
 };
 
+// Функция рандомных чисел
 function randomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     rand = Math.round(rand);
